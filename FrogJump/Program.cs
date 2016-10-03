7245fd5ef754756a0b32ec08eb0025f9d3e92f92
Minh Tran <quocminh2303@gmail.com>
2016-10-03 14:20:56 +0200

    update

---
+++ FrogJump/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrogJump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = 25;
+            int Y = 80;
+            int D = 10;
+            int result = 1;
+            while (X+D < Y)
+            {
+                X += D;
+                result++;
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
